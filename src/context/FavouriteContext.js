fd24fe32863eb3489259abb0b094b9c48bf215bf
Adam Grodzki <110930032+AdamGrodzki@users.noreply.github.com>
2024-03-26 14:47:44 +0100

    initial commit

---
+++ src/context/FavouriteContext.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+
+// const FavouriteContext = React.createContext({
+//     favouritePokemons: [],
+//     updateFavouritePokemons: (id) => null
+// });
+
+// export const FavoriteProvider = FavouriteContext.Provider;
+
+// export default FavouriteContext;
---
+++ src/styles/colors.js
@@ -0,0 +1,10 @@
+const Colors = {
+    dark: "#212529",
+    green: "#13ce66",
+    pink: "#fe8bc4",
+    red: "#ff0000 ",
+    yellow: "#ffcb2c",
+    orange: "#fd7d24",
+};
+
+export default Colors;
