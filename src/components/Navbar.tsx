5f93e9f30e1d2ade3abaf56aa89ecf2d724fbdd4
Adam Grodzki <110930032+AdamGrodzki@users.noreply.github.com>
2024-04-29 15:34:26 +0200

    add function show/hidden for navbar mobile device

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,24 @@
 import { Link, useLocation } from "react-router-dom";
 import "../styles/navbar.css"
+import { useState } from "react";
 
 export const Navbar = () => {
     const location = useLocation();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
         <nav className="navbar">
-              <input type="checkbox" id="burger-menu" className="burger-menu-checkbox" />
+              <input type="checkbox" id="burger-menu" className="burger-menu-checkbox" checked={isMenuOpen} onChange={toggleMenu} />
   <label htmlFor="burger-menu" className="burger-menu-label">
     <span className="burger-menu-icon"></span>
   </label>
-            <ul className="nav-link-container">
-                <li><Link className={`nav-link ${location.pathname === "/" ? "active" : ''}`} to="/">Home</Link></li>
-                <li><Link className={`nav-link ${location.pathname === "/favourite" ? "active" : ''}`} to="/favourite">Favourites</Link></li>
+            <ul className={`nav-link-container ${isMenuOpen ? "show" : ""}`}>
+                <li><Link className={`nav-link ${location.pathname === "/" ? "active" : ''}`} to="/" onClick={toggleMenu}>Home</Link></li>
+                <li><Link className={`nav-link ${location.pathname === "/favourite" ? "active" : ''}`} to="/favourite" onClick={toggleMenu}>Favourites</Link></li>
             </ul>
         </nav>
     );
